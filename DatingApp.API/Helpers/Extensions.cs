20fcf264de7f331ca01dff50fab5decba81768a7
Mehrad Sharifi <power.electrical1879@gmail.com>
2019-09-10 15:18:17 +0430

    End of Section 14

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,23 +1,34 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace DatingApp.API.Helpers
 {
-    public static class Extensions
+  public static class Extensions
+  {
+    public static void AddApplicationError(this HttpResponse response, string message)
     {
-        public static void AddApplicationError(this HttpResponse response, string message)
-        {
-            response.Headers.Add("Application-Eror", message);
-            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
-            response.Headers.Add("Access-Control-Allow_Origin", "*");
-        }
-        public static int CalculateAge(this DateTime theDateTime)
-        {
-            var age = DateTime.Today.Year - theDateTime.Year;
-            if (theDateTime.AddYears(age) > DateTime.Today)
-                age--;
-
-            return age;
-        }
+      response.Headers.Add("Application-Eror", message);
+      response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+      response.Headers.Add("Access-Control-Allow_Origin", "*");
     }
+
+    public static void AddPagination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItmes, int totalPages)
+    {
+      var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItmes, totalPages);
+      var camelCaseFormatter = new JsonSerializerSettings();
+      camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+      response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+    }
+    public static int CalculateAge(this DateTime theDateTime)
+    {
+      var age = DateTime.Today.Year - theDateTime.Year;
+      if (theDateTime.AddYears(age) > DateTime.Today)
+        age--;
+
+      return age;
+    }
+  }
 }
